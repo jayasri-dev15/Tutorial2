0790629d1a6fa2c92f2d39964670a01845929684
Jayasri R <77390130+jayasri-dev15@users.noreply.github.com>
2021-06-30 11:20:57 +0530

    updated the test steps

--- Basics/src/com/sample/Welcome.java
+++ Basics/src/com/sample/Welcome.java
@@ -10,3 +10,15 @@ public class Welcome {
 	}
 
 }
+/*package com.sample;
+
+public class Welcome {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello all");
+
+	}
+
+}*/
